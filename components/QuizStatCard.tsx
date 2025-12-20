bb46bd8fed28f171f86970ee2a4f3184ae78d220
Nikko Puno <punojohnnikkor@gmail.com>
2025-12-21 03:55:05 +0800

    fixed again

--- components/QuizStatCard.tsx
+++ components/QuizStatCard.tsx
@@ -9,7 +9,7 @@ interface QuizStatCardProp {
 function QuizStatCard({title, value}: QuizStatCardProp) {
   return (
     <div className='card mt-10 py-5 pl-4 pr-50 '>
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col gap-4'>
             <h3 className='text-muted'>{title}</h3>
             <p className='text-4xl font-semibold'>{value}</p>
         </div> 
